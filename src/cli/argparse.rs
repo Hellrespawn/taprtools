4fd91834a97b943e0f8146e20326dd410c57407b
Stef Korporaal <stefkorporaal@gmail.com>
2021-07-02 21:12:47 +0200

    Latest

--- src/cli/argparse.rs
+++ src/cli/argparse.rs
@@ -5,15 +5,63 @@ use clap::{load_yaml, App, ArgMatches};
 
 #[derive(Debug)]
 pub enum SubArgs {
-    Undo { amount: u64 },
-    Redo { amount: u64 },
-    Inspect { name: String },
+    Undo {
+        amount: u64,
+    },
+    Redo {
+        amount: u64,
+    },
+    Inspect {
+        name: String,
+    },
     Rename {
         name: String,
         arguments: Option<Vec<String>>,
         recursive: bool,
-        allow_case_difference: bool
+        allow_case_difference: bool,
     },
+}
+
+impl SubArgs {
+    fn from_subcommand(name: &str, submatches: &ArgMatches) -> Option<Self> {
+        match name {
+            "undo" => Some(SubArgs::Undo {
+                amount: submatches
+                    .value_of("amount")
+                    .unwrap()
+                    .parse::<u64>()
+                    .expect("Invalid amount!"),
+            }),
+            "redo" => Some(SubArgs::Redo {
+                amount: submatches
+                    .value_of("amount")
+                    .unwrap()
+                    .parse::<u64>()
+                    .expect("Invalid amount!"),
+            }),
+            "inspect" => Some(SubArgs::Inspect {
+                name: submatches
+                    .value_of("name")
+                    .expect("Name wasn't specified!")
+                    .to_string(),
+            }),
+            "rename" => Some(SubArgs::Rename {
+                name: submatches
+                    .value_of("name")
+                    .expect("Name wasn't specified!")
+                    .to_string(),
+                // Option::map maps Option<T> to Option<U>
+                // Iterator::map items in iterator
+                arguments: submatches
+                    .values_of("arguments")
+                    .map(|i| i.map(String::from).collect()),
+                allow_case_difference: submatches
+                    .is_present("allow-case-difference"),
+                recursive: submatches.is_present("recursive"),
+            }),
+            _ => None,
+        }
+    }
 }
 
 #[derive(Debug, Default)]
@@ -40,43 +88,12 @@ pub fn parse_args() -> Result<()> {
 
     if let Some(name) = matches.subcommand_name() {
         let submatches = matches.subcommand_matches(name).unwrap();
-        args.accumulate_ArgMatches(&submatches);
 
-        args.sub_args = match name {
-            "undo" => Some(parse_undo(&submatches)),
-            "redo" => Some(parse_redo(&submatches)),
-            "inspect" => Some(parse_inspect(&submatches)),
-            "rename" => Some(parse_rename(&submatches)),
-            _ => None,
-        };
+        args.accumulate_ArgMatches(&submatches);
+        args.sub_args = SubArgs::from_subcommand(name, submatches);
     }
 
     println!("{:#?}", args);
 
     Ok(())
-}
-
-fn parse_undo(submatches: &ArgMatches) -> SubArgs {
-    // Has default in ttf.yaml, so we can unwrap safely.
-    SubArgs::Undo { amount: submatches.value_of("amount").unwrap().parse::<u64>().expect("Invalid amount!") }
-}
-
-fn parse_redo(submatches: &ArgMatches) -> SubArgs {
-    // Has default in ttf.yaml, so we can unwrap safely.
-    SubArgs::Redo { amount: submatches.value_of("amount").unwrap().parse::<u64>().expect("Invalid amount!") }
-}
-
-fn parse_inspect(submatches: &ArgMatches) -> SubArgs {
-    SubArgs::Inspect{ name: submatches.value_of("name").expect("Name wasn't specified!").to_string() }
-}
-
-fn parse_rename(submatches: &ArgMatches) -> SubArgs {
-    SubArgs::Rename {
-        name: submatches.value_of("name").expect("Name wasn't specified!").to_string(),
-        // Option::map maps Option<T> to Option<U>
-        // Iterator::map items in iterator
-        arguments: submatches.values_of("arguments").map(|i| i.map(String::from).collect()),
-        allow_case_difference: submatches.is_present("allow-case-difference"),
-        recursive: submatches.is_present("recursive"),
-    }
 }
