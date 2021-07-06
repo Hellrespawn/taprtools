0f09e69cc263b79d69ed6752146f0b690c481553
Stef Korporaal <stefkorporaal@gmail.com>
2021-07-06 20:12:04 +0200

    Refactor to work with new visitor that returns values.

--- src/tfmt/genastdot.rs
+++ src/tfmt/genastdot.rs
@@ -1,4 +1,3 @@
-// TODO Refactor to work with new visitor that returns values.
 use super::ast::{self, Expression, Node};
 use super::token::TOKEN_TYPE_STRING_MAP;
 use super::visitor::Visitor;
@@ -12,8 +11,8 @@ use std::process::Command;
 
 use anyhow::Result;
 
+#[derive(Default)]
 pub struct GenAstDot {
-    dot_body: String,
     counter: u64,
 }
 
@@ -23,9 +22,7 @@ pub fn visualize_ast(
     name: &str,
     remove_dot_file: bool,
 ) -> Result<()> {
-    let mut g = GenAstDot::new(String::new());
-
-    root.accept(&mut g);
+    let mut g: GenAstDot = Default::default();
 
     let mut dot = "digraph astgraph {\n  \
         edge [arrowsize=.5];\n  \
@@ -35,7 +32,7 @@ pub fn visualize_ast(
         ranksep=0.75;\n  "
         .to_owned();
 
-    dot.push_str(&g.dot_body);
+    dot.push_str(&root.accept(&mut g));
 
     dot.push('}');
 
@@ -68,19 +65,12 @@ pub fn visualize_ast(
 }
 
 impl GenAstDot {
-    pub fn new(dot_body: String) -> GenAstDot {
-        GenAstDot {
-            dot_body,
-            counter: 0,
-        }
-    }
-
     fn increment(&mut self) -> u64 {
         self.counter += 1;
         self.counter - 1
     }
 
-    fn create_node(&mut self, label: &str, hidden: bool) -> u64 {
+    fn create_node(&mut self, label: &str, hidden: bool) -> (String, u64) {
         let label_str = {
             if hidden {
                 "[shape=point]".to_owned()
@@ -89,17 +79,14 @@ impl GenAstDot {
             }
         };
 
-        self.dot_body
-            .push_str(&format!("  node{} {}\n", self.counter, label_str));
-
-        self.increment()
+        (format!("  node{} {}\n", self.counter, label_str), self.increment())
     }
 
-    fn new_node(&mut self, label: &str) -> u64 {
+    fn new_node(&mut self, label: &str) -> (String, u64) {
         self.create_node(label, false)
     }
 
-    fn hidden_node(&mut self) -> u64 {
+    fn hidden_node(&mut self) -> (String, u64) {
         self.create_node("", true)
     }
 
@@ -109,7 +96,7 @@ impl GenAstDot {
         node2: u64,
         label: Option<&str>,
         directed: bool,
-    ) {
+    ) -> String {
         let mut string = format!("  node{} -> node{}", node2, node1);
 
         let mut args: Vec<String> = Vec::new();
@@ -128,10 +115,10 @@ impl GenAstDot {
 
         string.push('\n');
 
-        self.dot_body.push_str(&string);
+        string
     }
 
-    fn connect_nodes(&mut self, node1: u64, node2: u64) {
+    fn connect_nodes(&mut self, node1: u64, node2: u64) -> String {
         self.node_connector(node1, node2, None, true)
     }
 
@@ -140,15 +127,14 @@ impl GenAstDot {
         node1: u64,
         node2: u64,
         label: &str,
-    ) {
+    ) -> String {
         self.node_connector(node1, node2, Some(label), true)
     }
 }
 
-impl Visitor<()> for GenAstDot {
-    //type Result;
-    fn visit_program(&mut self, program: &ast::Program) {
-        let program_node = self.new_node(&format!(
+impl Visitor<String> for GenAstDot {
+    fn visit_program(&mut self, program: &ast::Program) -> String {
+        let (mut string, program_node) = self.new_node(&format!(
             "Program\n{}",
             program
                 .name
@@ -157,36 +143,40 @@ impl Visitor<()> for GenAstDot {
                 .expect("Name required for program!")
         ));
 
-        self.dot_body
-            .push_str("subgraph header {\nrankdir=\"RL\";\n");
+        string += "subgraph header {\nrankdir=\"RL\";\n";
 
         let parameters_node = self.counter;
-        program.parameters.accept(self);
-        self.connect_nodes(parameters_node, program_node);
 
-        self.dot_body
-            .push_str("}\nsubgraph block  {\nrankdir=\"LR\";\n");
+        string += &program.parameters.accept(self);
+
+        string += &self.connect_nodes(parameters_node, program_node);
+
+        string += "}\nsubgraph block  {\nrankdir=\"LR\";\n";
 
         let block_node = self.counter;
-        program.block.accept(self);
-        self.connect_nodes(block_node, program_node);
+        string += &program.block.accept(self);
+        string += &self.connect_nodes(block_node, program_node);
 
-        self.dot_body.push_str("}\n");
+        string += "}\n";
+
+        string
     }
 
-    fn visit_parameters(&mut self, parameters: &ast::Parameters) {
-        let parameters_node = self
+    fn visit_parameters(&mut self, parameters: &ast::Parameters) -> String {
+        let (mut string, parameters_node) = self
             .new_node(&format!("Params:\n({})", parameters.parameters.len()));
 
         for parameter in parameters.parameters.iter() {
             let parameter_node = self.counter;
-            parameter.accept(self);
-            self.connect_nodes(parameter_node, parameters_node);
+            string += &parameter.accept(self);
+            string += &self.connect_nodes(parameter_node, parameters_node);
         }
+
+        string
     }
 
-    fn visit_parameter(&mut self, parameter: &ast::Parameter) {
-        let parameter_node = self.new_node(
+    fn visit_parameter(&mut self, parameter: &ast::Parameter) -> String {
+        let (mut string, parameter_node) = self.new_node(
             parameter
                 .token
                 .value
@@ -195,31 +185,36 @@ impl Visitor<()> for GenAstDot {
         );
 
         if let Some(default) = parameter.default.as_ref() {
-            let default_node = self.new_node(
+            let (default_string, default_node) = self.new_node(
                 default
                     .value
                     .as_ref()
                     .expect("Parameter default token must have value!"),
             );
-            self.connect_nodes(default_node, parameter_node);
+
+            string += &default_string;
+            string += &self.connect_nodes(default_node, parameter_node);
         }
+
+        string
     }
 
-    fn visit_block(&mut self, block: &ast::Block) {
-        let block_node = self.new_node("Block");
+    fn visit_block(&mut self, block: &ast::Block) -> String {
+        let (mut string, block_node) = self.new_node("Block");
 
         if let Some(drive) = &block.drive {
             let drive_node = self.counter;
-            drive.accept(self);
-            self.connect_nodes_with_label(
+            string += &drive.accept(self);
+            string += &self.connect_nodes_with_label(
                 drive_node,
                 block_node,
                 "drive letter",
             );
         }
 
-        let expressions_node = self.hidden_node();
-        self.node_connector(
+        let (expressions_string, expressions_node) = self.hidden_node();
+        string += &expressions_string;
+        string += &self.node_connector(
             expressions_node,
             block_node,
             Some("expressions"),
@@ -228,13 +223,15 @@ impl Visitor<()> for GenAstDot {
 
         for expression in block.expressions.iter() {
             let expression_node = self.counter;
-            expression.accept(self);
-            self.connect_nodes(expression_node, expressions_node)
+            string += &expression.accept(self);
+            string += &self.connect_nodes(expression_node, expressions_node)
         }
+
+        string
     }
 
-    fn visit_driveletter(&mut self, driveletter: &ast::DriveLetter) {
-        self.new_node(&format!(
+    fn visit_driveletter(&mut self, driveletter: &ast::DriveLetter) -> String {
+        let (string, _) = self.new_node(&format!(
             "Drive: {}:\\",
             driveletter
                 .token
@@ -242,9 +239,11 @@ impl Visitor<()> for GenAstDot {
                 .as_ref()
                 .expect("Token in DriveLetter must have value!")
         ));
+
+        string
     }
 
-    fn visit_expression(&mut self, expression: &Expression) {
+    fn visit_expression(&mut self, expression: &Expression) -> String {
         match expression {
             Expression::TernaryOp {
                 condition,
@@ -279,20 +278,22 @@ impl GenAstDot {
         condition: &Expression,
         true_expr: &Expression,
         false_expr: &Expression,
-    ) {
-        let ternaryop_node = self.new_node("TernOp:\n\'?:\'");
+    ) -> String {
+        let (mut string, ternaryop_node) = self.new_node("TernOp:\n\'?:\'");
 
         let condition_node = self.counter;
-        condition.accept(self);
-        self.connect_nodes_with_label(condition_node, ternaryop_node, "cond");
+        string += &condition.accept(self);
+        string += &self.connect_nodes_with_label(condition_node, ternaryop_node, "cond");
 
         let true_node = self.counter;
-        true_expr.accept(self);
-        self.connect_nodes_with_label(true_node, ternaryop_node, "cond");
+        string += &true_expr.accept(self);
+        string += &self.connect_nodes_with_label(true_node, ternaryop_node, "cond");
 
         let false_node = self.counter;
-        false_expr.accept(self);
-        self.connect_nodes_with_label(false_node, ternaryop_node, "cond");
+        string += &false_expr.accept(self);
+        string += &self.connect_nodes_with_label(false_node, ternaryop_node, "cond");
+
+        string
     }
 
     fn visit_binaryop(
@@ -300,48 +301,54 @@ impl GenAstDot {
         left: &Expression,
         token: &Token,
         right: &Expression,
-    ) {
-        let binaryop_node = self.new_node(&format!(
+    ) -> String {
+        let (mut string, binaryop_node) = self.new_node(&format!(
             "BinOp:\n{}",
             TOKEN_TYPE_STRING_MAP.get_by_left(&token.ttype).unwrap()
         ));
 
         let left_node = self.counter;
-        left.accept(self);
-        self.connect_nodes(left_node, binaryop_node);
+        string += &left.accept(self);
+        string += &self.connect_nodes(left_node, binaryop_node);
 
         let right_node = self.counter;
-        right.accept(self);
-        self.connect_nodes(right_node, binaryop_node);
+        string += &right.accept(self);
+        string += &self.connect_nodes(right_node, binaryop_node);
+
+        string
     }
 
-    fn visit_unaryop(&mut self, token: &Token, operand: &Expression) {
-        let unaryop_node = self.new_node(&format!(
+    fn visit_unaryop(&mut self, token: &Token, operand: &Expression) -> String {
+        let (mut string, unaryop_node) = self.new_node(&format!(
             "UnOp:\n{}",
             TOKEN_TYPE_STRING_MAP.get_by_left(&token.ttype).unwrap()
         ));
 
         let operand_node = self.counter;
-        operand.accept(self);
-        self.connect_nodes(operand_node, unaryop_node);
+        string += &operand.accept(self);
+        string += &self.connect_nodes(operand_node, unaryop_node);
+
+        string
     }
 
-    fn visit_group(&mut self, expressions: &[Expression]) {
-        let group_node = self.new_node("Group\n\'(...)\'");
+    fn visit_group(&mut self, expressions: &[Expression]) -> String {
+        let (mut string, group_node) = self.new_node("Group\n\'(...)\'");
 
         for expression in expressions.iter() {
             let expression_node = self.counter;
-            expression.accept(self);
-            self.connect_nodes(expression_node, group_node);
+            string += &expression.accept(self);
+            string += &self.connect_nodes(expression_node, group_node);
         }
+
+        string
     }
 
     fn visit_function(
         &mut self,
         start_token: &Token,
         arguments: &[Expression],
-    ) {
-        let function_node = self.new_node(&format!(
+    ) -> String {
+        let (mut string, function_node) = self.new_node(&format!(
             "Function:\n${}(...)",
             start_token
                 .value
@@ -351,50 +358,60 @@ impl GenAstDot {
 
         for (i, expression) in arguments.iter().enumerate() {
             let expression_node = self.counter;
-            expression.accept(self);
-            self.connect_nodes_with_label(
+            string += &expression.accept(self);
+            string += &self.connect_nodes_with_label(
                 expression_node,
                 function_node,
                 &format!("a{}", i + 1),
             );
         }
+
+        string
     }
 
-    fn visit_integer(&mut self, integer: &Token) {
-        self.new_node(&format!(
+    fn visit_integer(&mut self, integer: &Token) -> String {
+        let (string, _) = self.new_node(&format!(
             "Int:\n{}",
             integer
                 .value
                 .as_ref()
                 .expect("Token in Integer must have value!")
         ));
+
+        string
     }
 
-    fn visit_string(&mut self, string: &Token) {
-        //TODO trim string
-        self.new_node(&format!(
+    fn visit_string(&mut self, string: &Token) -> String {
+        // TODO trim string
+        let (string, _) = self.new_node(&format!(
             "String:\n{}",
             string
                 .value
                 .as_ref()
                 .expect("Token in StringNode must have value!")
         ));
+
+        string
     }
 
-    fn visit_substitution(&mut self, substitution: &Token) {
-        self.new_node(&format!(
+    fn visit_substitution(&mut self, substitution: &Token) -> String {
+        let (string, _) = self.new_node(&format!(
             "Sub:\n{}",
             substitution
                 .value
                 .as_ref()
                 .expect("Token in Substitution must have value!")
         ));
+
+        string
     }
 
-    fn visit_tag(&mut self, token: &Token) {
-        self.new_node(&format!(
+    fn visit_tag(&mut self, token: &Token) -> String {
+        let (string, _) = self.new_node(&format!(
             "Tag:\n<{}>",
             token.value.as_ref().expect("Token in Tag must have value!")
         ));
+
+        string
     }
 }
