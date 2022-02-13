fb4818c3b86b06452f6b4c0b79404b705eead4f5
Stef Korporaal <stefkorporaal@gmail.com>
2022-02-13 15:28:37 +0100

    Comment out old test_file_history

--- file-history/tests/test_file_history.rs
+++ file-history/tests/test_file_history.rs
@@ -1,17 +1,17 @@
-use anyhow::{bail, Result};
-use tempfile::{Builder, TempDir};
+// use anyhow::{bail, Result};
+// use tempfile::{Builder, TempDir};
 
-fn setup_environment(suffix: Option<&str>) -> Result<TempDir> {
-    let t = if let Some(suffix) = suffix {
-        Builder::new()
-            .prefix("file-history-")
-            .suffix(&("-".to_string() + suffix))
-            .tempdir()?
-    } else {
-        Builder::new().prefix("file-history-").tempdir()?
-    };
+// fn setup_environment(suffix: Option<&str>) -> Result<TempDir> {
+//     let t = if let Some(suffix) = suffix {
+//         Builder::new()
+//             .prefix("file-history-")
+//             .suffix(&("-".to_string() + suffix))
+//             .tempdir()?
+//     } else {
+//         Builder::new().prefix("file-history-").tempdir()?
+//     };
 
-    Ok(t)
-}
+//     Ok(t)
+// }
 
-// TODO Write tests for file-history
+// // TODO Write tests for file-history
